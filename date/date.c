910676c5d65cdb2c0673e5656595e5aad2aff5d7
Kamran Amini <kam.cpp@gmail.com>
2015-08-23 16:39:54 +0430

    Headers have been removed.

--- date/date.c
+++ date/date.c
@@ -49,6 +49,14 @@ NAEEM_date__add_years_to_jalali_date(NAEEM_date__jalali_date_ptr jalali_date_ptr
 }
 
 
+NAEEM_unix_epoch
+NAEEM_date__get_current_unix_epoch() {
+  NAEEM_date__gregorian_date gregorian_date;
+  NAEEM_date__get_current_gregorian_date(&gregorian_date);
+  return NAEEM_date__get_epoch_from_gregorian_date(gregorian_date);
+}
+
+
 NAEEM_void
 NAEEM_date__get_current_gregorian_date(NAEEM_date__gregorian_date_ptr gregorian_date_ptr) {
   time_t t = time(NULL);
