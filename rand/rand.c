1bd3fd8bdaff08c2254d5f69372c31060437e699
Kamran Amini <kam.cpp@gmail.com>
2016-05-05 23:42:30 +0430

    Rand dir has been refactored.

--- rand/rand.c
+++ rand/rand.c
@@ -1,8 +1,33 @@
-#include <naeem/rand.h>
+/*  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 LabCrypto Org.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <org/labcrypto/abettor/rand.h>
 
 
-NAEEM_void
-NAEEM_rand__generate_8_bytes (NAEEM_data buffer) {
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__rand__generate_8_bytes (
+  ORG_LABCRYPTO_ABETTOR_data buffer
+) {
   buffer[0] = 0x78;
   buffer[1] = 0x17;
   buffer[2] = 0x23;
@@ -14,8 +39,10 @@ NAEEM_rand__generate_8_bytes (NAEEM_data buffer) {
 }
 
 
-NAEEM_void
-NAEEM_rand__generate_16_bytes (NAEEM_data buffer) {
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__rand__generate_16_bytes (
+  ORG_LABCRYPTO_ABETTOR_data buffer
+) {
   buffer[0] = 0x0b;
   buffer[1] = 0x79;
   buffer[2] = 0x52;
