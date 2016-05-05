a9d100a0129cd8fc261566adff839fc1b42ba4f6
Kamran Amini <kam.cpp@gmail.com>
2016-05-06 01:25:49 +0430

    Util dir has been refactored.

--- util/util.c
+++ util/util.c
@@ -1,26 +1,53 @@
+/*  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 LabCrypto Org.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 #include <string.h>
 
-#include <naeem/util.h>
+#include <org/labcrypto/abettor/util.h>
 
 
-NAEEM_void
-NAEEM_util__copy_array (NAEEM_data destination,
-                        NAEEM_data source,
-                        NAEEM_uint32 start_index,
-                        NAEEM_uint32 length) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__copy_array (
+  ORG_LABCRYPTO_ABETTOR_data destination,
+  ORG_LABCRYPTO_ABETTOR_data source,
+  ORG_LABCRYPTO_ABETTOR_uint32 start_index,
+  ORG_LABCRYPTO_ABETTOR_uint32 length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   for (; i < length; i++) {
     destination[i] = source[start_index + i];
   }
 }
 
 
-NAEEM_void
-NAEEM_util__print_array (NAEEM_string title,
-                         NAEEM_data data, 
-                         NAEEM_uint32 data_length) {
-  NAEEM_uint32 title_length = 0;
-  NAEEM_uint32 i = 0, j = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__print_array (
+  ORG_LABCRYPTO_ABETTOR_string title,
+  ORG_LABCRYPTO_ABETTOR_data data, 
+  ORG_LABCRYPTO_ABETTOR_uint32 data_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 title_length = 0;
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0, j = 0;
   if (title) {
     printf("%s: ", title);
     title_length = strlen((const char*)title) + 2;
@@ -40,12 +67,14 @@ NAEEM_util__print_array (NAEEM_string title,
 }
 
 
-NAEEM_bool
-NAEEM_util__compare_data (NAEEM_data first_data, 
-                          NAEEM_uint32 first_data_length, 
-                          NAEEM_data second_data,
-                          NAEEM_uint32 second_data_length) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_bool
+ORG_LABCRYPTO_ABETTOR__util__compare_data (
+  ORG_LABCRYPTO_ABETTOR_data first_data, 
+  ORG_LABCRYPTO_ABETTOR_uint32 first_data_length, 
+  ORG_LABCRYPTO_ABETTOR_data second_data,
+  ORG_LABCRYPTO_ABETTOR_uint32 second_data_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   if (first_data_length != second_data_length) {
     return FALSE;
   }
@@ -58,21 +87,25 @@ NAEEM_util__compare_data (NAEEM_data first_data,
 }
 
 
-NAEEM_void
-NAEEM_util__concat_data (NAEEM_data destination,
-                         NAEEM_uint32 start_index,           
-                         NAEEM_data source,
-                         NAEEM_uint32 source_length) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__concat_data (
+  ORG_LABCRYPTO_ABETTOR_data destination,
+  ORG_LABCRYPTO_ABETTOR_uint32 start_index,           
+  ORG_LABCRYPTO_ABETTOR_data source,
+  ORG_LABCRYPTO_ABETTOR_uint32 source_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   for (; i < source_length; i++) {
     destination[i + start_index] = source[i];
   }  
 }
 
 
-NAEEM_string
-NAEEM_util__trim_string (NAEEM_string str) {
-  NAEEM_char *end;
+ORG_LABCRYPTO_ABETTOR_string
+ORG_LABCRYPTO_ABETTOR__util__trim_string (
+  ORG_LABCRYPTO_ABETTOR_string str
+) {
+  ORG_LABCRYPTO_ABETTOR_char *end;
   while (*str == ' ' || *str == '\t' || *str == '\r' || *str == '\n') {
     str++;
   }
@@ -89,21 +122,25 @@ NAEEM_util__trim_string (NAEEM_string str) {
 }
 
 
-NAEEM_void
-NAEEM_util__xor (NAEEM_data destination,    
-                 NAEEM_uint32 destination_length,                      
-                 NAEEM_data source) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__xor (
+  ORG_LABCRYPTO_ABETTOR_data destination,    
+  ORG_LABCRYPTO_ABETTOR_uint32 destination_length,                      
+  ORG_LABCRYPTO_ABETTOR_data source
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   for (; i < destination_length; i++) {
     destination[i] = destination[i] ^ source[i];
   }
 }
 
 
-NAEEM_void
-NAEEM_util__increment (NAEEM_data number,
-                       NAEEM_uint32 number_length) {
-  NAEEM_uint32 i = number_length - 1;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__increment (
+  ORG_LABCRYPTO_ABETTOR_data number,
+  ORG_LABCRYPTO_ABETTOR_uint32 number_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = number_length - 1;
   for (; i >= 0; i--) {
     if (number[i] == 255) {
       number[i] = 0;
@@ -115,25 +152,29 @@ NAEEM_util__increment (NAEEM_data number,
 }
 
 
-NAEEM_void
-NAEEM_util__make_zero (NAEEM_data buffer,
-                       NAEEM_uint32 buffer_length) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__make_zero (
+  ORG_LABCRYPTO_ABETTOR_data buffer,
+  ORG_LABCRYPTO_ABETTOR_uint32 buffer_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   for (i = 0; i < buffer_length; i++) {
     buffer[i] = 0x00;
   }
 }
 
 
-NAEEM_void
-NAEEM_util__pad (NAEEM_data buffer,
-                 NAEEM_uint32 buffer_length,
-                 NAEEM_data_ptr padded_buffer,
-                 NAEEM_uint32_ptr padded_buffer_length) {
-  NAEEM_uint32 i = 0;
-  NAEEM_uint32 new_length = (buffer_length % 8) == 0 ? buffer_length + 8 : ((buffer_length / 8) + 1) * 8;
-  *padded_buffer = (NAEEM_data)malloc(new_length * sizeof(NAEEM_byte));
-  NAEEM_util__make_zero(*padded_buffer, new_length);
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__pad (
+  ORG_LABCRYPTO_ABETTOR_data buffer,
+  ORG_LABCRYPTO_ABETTOR_uint32 buffer_length,
+  ORG_LABCRYPTO_ABETTOR_data_ptr padded_buffer,
+  ORG_LABCRYPTO_ABETTOR_uint32_ptr padded_buffer_length
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
+  ORG_LABCRYPTO_ABETTOR_uint32 new_length = (buffer_length % 8) == 0 ? buffer_length + 8 : ((buffer_length / 8) + 1) * 8;
+  *padded_buffer = (ORG_LABCRYPTO_ABETTOR_data)malloc(new_length * sizeof(ORG_LABCRYPTO_ABETTOR_byte));
+  ORG_LABCRYPTO_ABETTOR__util__make_zero(*padded_buffer, new_length);
   
   for (; i < buffer_length; i++) {
     (*padded_buffer)[i] = buffer[i];
@@ -145,12 +186,14 @@ NAEEM_util__pad (NAEEM_data buffer,
 }
 
 
-NAEEM_string
-NAEEM_util__array_to_string (NAEEM_data array, 
-                             NAEEM_uint32 length) {
-  NAEEM_char temp[3];
-  NAEEM_uint32 i = 0;
-  NAEEM_string result = (NAEEM_string)malloc((2 * length + 1) * sizeof(NAEEM_char));
+ORG_LABCRYPTO_ABETTOR_string
+ORG_LABCRYPTO_ABETTOR__util__array_to_string (
+  ORG_LABCRYPTO_ABETTOR_data array, 
+  ORG_LABCRYPTO_ABETTOR_uint32 length
+) {
+  ORG_LABCRYPTO_ABETTOR_char temp[3];
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
+  ORG_LABCRYPTO_ABETTOR_string result = (ORG_LABCRYPTO_ABETTOR_string)malloc((2 * length + 1) * sizeof(ORG_LABCRYPTO_ABETTOR_char));
   strcpy(result, "");
   for (; i < length; i++) {
     sprintf(temp, "%02x", array[i]);
@@ -160,9 +203,11 @@ NAEEM_util__array_to_string (NAEEM_data array,
 }
 
 
-NAEEM_void
-NAEEM_util__to_upper(NAEEM_string str) {
-  NAEEM_uint32 i = 0;
+ORG_LABCRYPTO_ABETTOR_void
+ORG_LABCRYPTO_ABETTOR__util__to_upper(
+  ORG_LABCRYPTO_ABETTOR_string str
+) {
+  ORG_LABCRYPTO_ABETTOR_uint32 i = 0;
   for (; i < strlen(str); i++) {
     if (str[i] >= 'a' && str[i] <= 'z') {
       str[i] = str[i] - 32;
